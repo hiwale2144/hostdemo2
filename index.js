aa0869c092fdbbae793588a657d250a14debec6a
shubham Hiwale <hiwale2144@gmail.com>
2021-12-02 14:07:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.use(require("./routes/auth"));
 
 const port = process.env.PORT || 8000;
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 app.listen(port, () => {
   console.log(`server ${port} are activated`);
 });
